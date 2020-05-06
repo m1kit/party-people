4d2ef5084ec549015b0cce784f9b90948f3684a3
mikit <mikihito0906@gmail.com>
2020-05-06 22:31:36 +0900

    Add rotation animation

--- src/index.ts
+++ src/index.ts
@@ -71,10 +71,10 @@ let h, w;
 let pose;
 let sourceCanvas: HTMLCanvasElement;
 let sourceRGBA, sourceRGB;
-let ones;
-let segmMask1, segmMask255, segmMaskNeg255;
-let fgSegmRGB, fgSegmHSV, fgSegmHSVVec, fgSegmHThresholdMask1, fgSegmHThresholdMask2;
-let bgRGB;
+let ones, saturator;
+let segmMaskFg, segmMaskBg, segmMaskTransformed;
+let fgSegmRGB, fgSegmHSV, fgSegmTransformed, fgSegmHSVVec, fgSegmHThresholdMask1, fgSegmHThresholdMask2;
+let outputRGB;
 
 async function initCV() {
     h = Math.floor(video.videoHeight * config.resolution.height);
@@ -87,51 +87,112 @@ async function initCV() {
     canvas.height = video.videoHeight;
     sourceRGBA = new cv.Mat(h, w, cv.CV_8UC4);
     sourceRGB = new cv.Mat(h, w, cv.CV_8UC3);
-    ones = new cv.Mat.ones(h, w, cv.CV_8UC1);
-    segmMask1 = new cv.Mat(h, w, cv.CV_8UC1);
-    segmMask255 = new cv.Mat(h, w, cv.CV_8UC1);
-    segmMaskNeg255 = new cv.Mat(h, w, cv.CV_8UC1);
+    ones = cv.Mat.ones(h, w, cv.CV_8UC1);
+    saturator = new cv.Mat(h, w, cv.CV_8UC1);
+    cv.multiply(ones, ones, saturator, config.saturation);
+    segmMaskFg = new cv.Mat(h, w, cv.CV_8UC1);
+    segmMaskBg = new cv.Mat(h, w, cv.CV_8UC1);
+    segmMaskTransformed = new cv.Mat(h, w, cv.CV_8UC1);
     fgSegmRGB = new cv.Mat(h, w, cv.CV_8UC3);
     fgSegmHSV = new cv.Mat(h, w, cv.CV_8UC3);
+    fgSegmTransformed = new cv.Mat(h, w, cv.CV_8UC3);
     fgSegmHSVVec = new cv.MatVector();
     fgSegmHThresholdMask1 = new cv.Mat(h, w, cv.CV_8UC1);
     fgSegmHThresholdMask2 = new cv.Mat(h, w, cv.CV_8UC1);
-    bgRGB = new cv.Mat(h, w, cv.CV_8UC3);
+    outputRGB = new cv.Mat(h, w, cv.CV_8UC3);
+}
+
+function beInRange(low, x, high) {
+    return Math.max(low, Math.min(x, high));
 }
 
 async function tick() {
-    // 1. Copy frame(Video -> Canvas -> Uint8Array -> cv.Mat[RBGA] -> cv.Mat[RGB])/
-    const ctx = sourceCanvas.getContext('2d');
-    ctx.drawImage(video, 0, 0, w, h);
-    sourceRGBA.data.set(ctx.getImageData(0, 0, w, h).data);
-    cv.cvtColor(sourceRGBA, sourceRGB, cv.COLOR_RGBA2RGB);
-
-    // 2. Segmentation
-    if (time % config.updateFrequency.bodypix === 0) {
-        const segment = await net.segmentPerson(sourceCanvas, config.bodypix.estimate);
-        pose = segment.allPoses[0];
-        segmMask1.data.set(segment.data);
-        cv.multiply(segmMask1, ones, segmMask255, 255);
-        cv.bitwise_not(segmMask255, segmMaskNeg255);
+    {   // 1. Copy frame(Video -> Canvas -> Uint8Array -> cv.Mat[RBGA] -> cv.Mat[RGB])
+        const ctx = sourceCanvas.getContext('2d');
+        ctx.drawImage(video, 0, 0, w, h);
+        sourceRGBA.data.set(ctx.getImageData(0, 0, w, h).data);
+        cv.cvtColor(sourceRGBA, sourceRGB, cv.COLOR_RGBA2RGB);
     }
-    sourceRGB.copyTo(fgSegmRGB, segmMask255);
-    fgSegmRGB.setTo(new cv.Scalar(0, 0, 0), segmMaskNeg255);
 
-    const d = time * config.speed.color % 180;
-    cv.cvtColor(fgSegmRGB, fgSegmHSV, cv.COLOR_RGB2HSV);
-    cv.split(fgSegmHSV, fgSegmHSVVec);
-    const fgSegmHue = fgSegmHSVVec.get(0);
-    cv.threshold(fgSegmHue, fgSegmHThresholdMask1, 179 - d, d, cv.THRESH_BINARY_INV);
-    cv.threshold(fgSegmHue, fgSegmHThresholdMask2, 179 - d, 180 - d, cv.THRESH_BINARY);
-    cv.add(fgSegmHue, fgSegmHThresholdMask1, fgSegmHue);
-    cv.subtract(fgSegmHue, fgSegmHThresholdMask2, fgSegmHue);
-    cv.merge(fgSegmHSVVec, fgSegmHSV);
+    {// 2. Segmentation
+        if (time % config.updateFrequency.bodypix === 0) {
+            const segment = await net.segmentPerson(sourceCanvas, config.bodypix.estimate);
+            pose = segment.allPoses[0];
+            segmMaskFg.data.set(segment.data);
+            cv.multiply(segmMaskFg, ones, segmMaskFg, 255);
+            cv.bitwise_not(segmMaskFg, segmMaskBg);
+        }
+        if (!pose || pose.keypoints[1].score < config.bodypix.threshold.eyes || pose.keypoints[2].score < config.bodypix.threshold.eyes) {
+            cv.imshow('canvas', sourceRGB); // cannot detect eyes
+            return;
+        }
+        fgSegmRGB.setTo(new cv.Scalar(0, 0, 0), segmMaskBg);
+        sourceRGB.copyTo(fgSegmRGB, segmMaskFg);
+    }
 
-    cv.cvtColor(fgSegmHSV, fgSegmRGB, cv.COLOR_HSV2RGB);
-    fgSegmRGB.copyTo(sourceRGB, segmMask255);
-    cv.imshow('canvas', sourceRGB);
+    {// 3. Paint
+        const d = time * config.speed.color % 180;
+        cv.cvtColor(fgSegmRGB, fgSegmHSV, cv.COLOR_RGB2HSV);
+        cv.split(fgSegmHSV, fgSegmHSVVec);
+        const fgSegmHue = fgSegmHSVVec.get(0);
+        cv.threshold(fgSegmHue, fgSegmHThresholdMask1, 179 - d, d, cv.THRESH_BINARY_INV);
+        cv.threshold(fgSegmHue, fgSegmHThresholdMask2, 179 - d, 180 - d, cv.THRESH_BINARY);
+        cv.add(fgSegmHue, fgSegmHThresholdMask1, fgSegmHue);
+        cv.subtract(fgSegmHue, fgSegmHThresholdMask2, fgSegmHue);
+        const fgSegmSaturation = fgSegmHSVVec.get(1);
+        cv.add(fgSegmSaturation, saturator, fgSegmSaturation);
+        cv.merge(fgSegmHSVVec, fgSegmHSV);
+        cv.cvtColor(fgSegmHSV, fgSegmRGB, cv.COLOR_HSV2RGB);
+    }
 
-    //cv.bitwise_and(sourceRGB, sourceRGB, fgSegmRGB, segmMask255);
-    //cv.bitwise_and(sourceRGB, sourceRGB, bgSegmRGB, segmMaskNeg255);
-    //bgSegmRGB.setTo(new cv.Scalar(0, 0, 0), segmMask255);
+    {// 4. Paint BG
+        sourceRGB.copyTo(outputRGB);
+    }
+
+    {// 5. Make distortion
+        const eyes = [pose.keypoints[1].position, pose.keypoints[2].position];
+        const center = { x: (eyes[0].x + eyes[1].x) / 2, y: (eyes[0].y + eyes[1].y) / 2 };
+        const nose = pose.keypoints[0].position;
+        const r = Math.hypot(nose.x - center.x, nose.y - center.y);
+        const th = Math.min(nose.y + r * config.faceRange, h - 1), bh = h - th; // top height, bottom height
+        const delta = {
+            x: beInRange(-w, config.rotationRadius.x * r * Math.cos(time * config.speed.rotation), w),
+            y: beInRange(th - h, config.rotationRadius.y * r * Math.sin(time * config.speed.rotation), h - th),
+        };
+        // bottom
+        const bottomSourceRect = new cv.Rect(0, th, w, bh);
+        const bottomDestRect = new cv.Rect(0, th + delta.y, w, bh - delta.y);
+        const bottomSourceROI = fgSegmRGB.roi(bottomSourceRect);
+        const bottomDestROI = fgSegmTransformed.roi(bottomDestRect);
+        const bottomSourceMaskROI = segmMaskFg.roi(bottomSourceRect);
+        const bottomDestMaskROI = segmMaskTransformed.roi(bottomDestRect);
+        const affineMat = cv.matFromArray(2, 3, cv.CV_64FC1, [1, -delta.x / bh, delta.x, 0, (bh - delta.y) / bh, 0]);
+        cv.warpAffine(bottomSourceROI, bottomDestROI, affineMat, new cv.Size(w, bh - delta.y), cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
+        cv.warpAffine(bottomSourceMaskROI, bottomDestMaskROI, affineMat, new cv.Size(w, bh - delta.y), cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
+        affineMat.delete();
+        bottomSourceROI.delete();
+        bottomDestROI.delete();
+        bottomSourceMaskROI.delete();
+        bottomDestMaskROI.delete();
+        // top
+        const topROISize = { x: w - Math.abs(delta.x), y: th - Math.max(0, -delta.y) };
+        const topSourceRect = new cv.Rect(Math.max(0, -delta.x), Math.max(0, -delta.y), topROISize.x, topROISize.y);
+        const topDestRect = new cv.Rect(Math.max(0, delta.x), Math.max(0, delta.y), topROISize.x, topROISize.y);
+        const topSourceROI = fgSegmRGB.roi(topSourceRect);
+        const topDestROI = fgSegmTransformed.roi(topDestRect);
+        const topSourceMaskROI = segmMaskFg.roi(topSourceRect);
+        const topDestMaskROI = segmMaskTransformed.roi(topDestRect)
+        topSourceROI.copyTo(topDestROI, topSourceMaskROI);
+        topSourceMaskROI.copyTo(topDestMaskROI);
+        topSourceROI.delete();
+        topDestROI.delete();
+        topSourceMaskROI.delete();
+        topDestMaskROI.delete();
+    }
+
+    {// 6. Show
+        fgSegmTransformed.copyTo(outputRGB, segmMaskTransformed)
+        cv.imshow('canvas', outputRGB);
+    }
 }
+
