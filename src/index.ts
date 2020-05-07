592621f6fe9377e827d658b3b30a9c20d3082cc7
mikit <mikihito0906@gmail.com>
2020-05-07 17:23:51 +0900

    Add space-bar trigger

--- src/index.ts
+++ src/index.ts
@@ -67,6 +67,13 @@ async function loop() {
     loop();
 })();
 
+let party = false;
+document.addEventListener("keydown", event => {
+    if (event.code === 'Space') {
+        party = !party;
+    }
+});
+
 let h, w;
 let pose;
 let sourceCanvas: HTMLCanvasElement;
@@ -153,18 +160,20 @@ async function tick() {
     }
 
     {// 3. Paint
-        const d = time * config.speed.color % 180;
-        cv.cvtColor(fgSegmRGB, fgSegmHSV, cv.COLOR_RGB2HSV);
-        cv.split(fgSegmHSV, fgSegmHSVVec);
-        const fgSegmHue = fgSegmHSVVec.get(0);
-        cv.threshold(fgSegmHue, fgSegmHThresholdMask1, 179 - d, d, cv.THRESH_BINARY_INV);
-        cv.threshold(fgSegmHue, fgSegmHThresholdMask2, 179 - d, 180 - d, cv.THRESH_BINARY);
-        cv.add(fgSegmHue, fgSegmHThresholdMask1, fgSegmHue);
-        cv.subtract(fgSegmHue, fgSegmHThresholdMask2, fgSegmHue);
-        const fgSegmSaturation = fgSegmHSVVec.get(1);
-        cv.add(fgSegmSaturation, saturator, fgSegmSaturation);
-        cv.merge(fgSegmHSVVec, fgSegmHSV);
-        cv.cvtColor(fgSegmHSV, fgSegmRGB, cv.COLOR_HSV2RGB);
+        if (party) {
+            const d = time * config.speed.color % 180;
+            cv.cvtColor(fgSegmRGB, fgSegmHSV, cv.COLOR_RGB2HSV);
+            cv.split(fgSegmHSV, fgSegmHSVVec);
+            const fgSegmHue = fgSegmHSVVec.get(0);
+            cv.threshold(fgSegmHue, fgSegmHThresholdMask1, 179 - d, d, cv.THRESH_BINARY_INV);
+            cv.threshold(fgSegmHue, fgSegmHThresholdMask2, 179 - d, 180 - d, cv.THRESH_BINARY);
+            cv.add(fgSegmHue, fgSegmHThresholdMask1, fgSegmHue);
+            cv.subtract(fgSegmHue, fgSegmHThresholdMask2, fgSegmHue);
+            const fgSegmSaturation = fgSegmHSVVec.get(1);
+            cv.add(fgSegmSaturation, saturator, fgSegmSaturation);
+            cv.merge(fgSegmHSVVec, fgSegmHSV);
+            cv.cvtColor(fgSegmHSV, fgSegmRGB, cv.COLOR_HSV2RGB);
+        }
     }
 
     {   // 4. Paint BG
@@ -177,44 +186,49 @@ async function tick() {
     }
 
     {// 5. Make distortion
-        const eyes = [pose.keypoints[1].position, pose.keypoints[2].position];
-        const center = { x: (eyes[0].x + eyes[1].x) / 2, y: (eyes[0].y + eyes[1].y) / 2 };
-        const nose = pose.keypoints[0].position;
-        const r = Math.hypot(nose.x - center.x, nose.y - center.y);
-        const th = Math.min(nose.y + r * config.faceRange, h - 1), bh = h - th; // top height, bottom height
-        const delta = {
-            x: beInRange(-w, config.rotationRadius.x * r * Math.cos(time * config.speed.rotation), w),
-            y: beInRange(th - h, config.rotationRadius.y * r * Math.sin(time * config.speed.rotation), h - th),
-        };
-        // bottom
-        const bottomSourceRect = new cv.Rect(0, th, w, bh);
-        const bottomDestRect = new cv.Rect(0, th + delta.y, w, bh - delta.y);
-        const bottomSourceROI = fgSegmRGB.roi(bottomSourceRect);
-        const bottomDestROI = outputFgRGB.roi(bottomDestRect);
-        const bottomSourceMaskROI = fgSegmMask.roi(bottomSourceRect);
-        const bottomDestMaskROI = segmMaskTransformed.roi(bottomDestRect);
-        const affineMat = cv.matFromArray(2, 3, cv.CV_64FC1, [1, -delta.x / bh, delta.x, 0, (bh - delta.y) / bh, 0]);
-        cv.warpAffine(bottomSourceROI, bottomDestROI, affineMat, new cv.Size(w, bh - delta.y), cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
-        cv.warpAffine(bottomSourceMaskROI, bottomDestMaskROI, affineMat, new cv.Size(w, bh - delta.y), cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
-        affineMat.delete();
-        bottomSourceROI.delete();
-        bottomDestROI.delete();
-        bottomSourceMaskROI.delete();
-        bottomDestMaskROI.delete();
-        // top
-        const topROISize = { x: w - Math.abs(delta.x), y: th - Math.max(0, -delta.y) };
-        const topSourceRect = new cv.Rect(Math.max(0, -delta.x), Math.max(0, -delta.y), topROISize.x, topROISize.y);
-        const topDestRect = new cv.Rect(Math.max(0, delta.x), Math.max(0, delta.y), topROISize.x, topROISize.y);
-        const topSourceROI = fgSegmRGB.roi(topSourceRect);
-        const topDestROI = outputFgRGB.roi(topDestRect);
-        const topSourceMaskROI = fgSegmMask.roi(topSourceRect);
-        const topDestMaskROI = segmMaskTransformed.roi(topDestRect)
-        topSourceROI.copyTo(topDestROI, topSourceMaskROI);
-        topSourceMaskROI.copyTo(topDestMaskROI);
-        topSourceROI.delete();
-        topDestROI.delete();
-        topSourceMaskROI.delete();
-        topDestMaskROI.delete();
+        if (party) {
+            const eyes = [pose.keypoints[1].position, pose.keypoints[2].position];
+            const center = { x: (eyes[0].x + eyes[1].x) / 2, y: (eyes[0].y + eyes[1].y) / 2 };
+            const nose = pose.keypoints[0].position;
+            const r = Math.hypot(nose.x - center.x, nose.y - center.y);
+            const th = Math.min(nose.y + r * config.faceRange, h - 1), bh = h - th; // top height, bottom height
+            const delta = {
+                x: beInRange(-w, config.rotationRadius.x * r * Math.cos(time * config.speed.rotation), w),
+                y: beInRange(th - h, config.rotationRadius.y * r * Math.sin(time * config.speed.rotation), h - th),
+            };
+            // bottom
+            const bottomSourceRect = new cv.Rect(0, th, w, bh);
+            const bottomDestRect = new cv.Rect(0, th + delta.y, w, bh - delta.y);
+            const bottomSourceROI = fgSegmRGB.roi(bottomSourceRect);
+            const bottomDestROI = outputFgRGB.roi(bottomDestRect);
+            const bottomSourceMaskROI = fgSegmMask.roi(bottomSourceRect);
+            const bottomDestMaskROI = segmMaskTransformed.roi(bottomDestRect);
+            const affineMat = cv.matFromArray(2, 3, cv.CV_64FC1, [1, -delta.x / bh, delta.x, 0, (bh - delta.y) / bh, 0]);
+            cv.warpAffine(bottomSourceROI, bottomDestROI, affineMat, new cv.Size(w, bh - delta.y), cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
+            cv.warpAffine(bottomSourceMaskROI, bottomDestMaskROI, affineMat, new cv.Size(w, bh - delta.y), cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
+            affineMat.delete();
+            bottomSourceROI.delete();
+            bottomDestROI.delete();
+            bottomSourceMaskROI.delete();
+            bottomDestMaskROI.delete();
+            // top
+            const topROISize = { x: w - Math.abs(delta.x), y: th - Math.max(0, -delta.y) };
+            const topSourceRect = new cv.Rect(Math.max(0, -delta.x), Math.max(0, -delta.y), topROISize.x, topROISize.y);
+            const topDestRect = new cv.Rect(Math.max(0, delta.x), Math.max(0, delta.y), topROISize.x, topROISize.y);
+            const topSourceROI = fgSegmRGB.roi(topSourceRect);
+            const topDestROI = outputFgRGB.roi(topDestRect);
+            const topSourceMaskROI = fgSegmMask.roi(topSourceRect);
+            const topDestMaskROI = segmMaskTransformed.roi(topDestRect)
+            topSourceROI.copyTo(topDestROI, topSourceMaskROI);
+            topSourceMaskROI.copyTo(topDestMaskROI);
+            topSourceROI.delete();
+            topDestROI.delete();
+            topSourceMaskROI.delete();
+            topDestMaskROI.delete();
+        } else {
+            fgSegmMask.copyTo(segmMaskTransformed);
+            fgSegmRGB.copyTo(outputFgRGB);
+        }
     }
 
     {// 6. Show
